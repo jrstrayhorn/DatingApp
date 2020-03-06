58ab777661bb85fa4f7c23054b3fc29b2b53d9c0
stepforwardtech <jrstrayhorn@gmail.com>
2020-03-06 06:58:20 -0500

    done creating lists component

--- DatingApp-SPA/src/app/lists/lists.component.ts
+++ DatingApp-SPA/src/app/lists/lists.component.ts
@@ -1,4 +1,9 @@
+import { UserService } from 'src/app/_services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { PaginatedResult } from '../_models/pagination';
+import { User } from '../_models/user';
+import { startWith, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-lists',
@@ -6,10 +11,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lists.component.css']
 })
 export class ListsComponent implements OnInit {
+  pageNumber = 1;
+  pageSize = 5;
+  likesParam = 'Likers';
 
-  constructor() { }
+  pageStream = new Subject<any>();
+
+  source$: Observable<PaginatedResult<User[]>>;
+
+  constructor(private userService: UserService) {}
 
   ngOnInit() {
+    this.source$ = this.pageStream.pipe(
+      startWith(this.pageNumber),
+      switchMap((page: number) => {
+        return this.userService.getUsers(
+          page,
+          this.pageSize,
+          null,
+          this.likesParam
+        );
+      })
+    );
   }
 
+  loadUsers(page?) {
+    const pageToLoad = page == null ? 1 : page;
+    this.pageStream.next(pageToLoad);
+  }
 }
